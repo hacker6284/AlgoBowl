e05ec3d98b6bc9e4593d77614aa7ab6aed9c9d61
ztmills <hacker.6284@gmail.com>
2018-09-24 17:03:56 -0600

    Deleted pretty much everything, updated .gitignore to ignore xcodeproj files

--- AlgoBowl/AlgoBowl/job.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  job.cpp
-//  AlgoBowl
-//
-//  Created by Tobias Pepper on 9/24/18.
-//  Copyright © 2018 CSCI406. All rights reserved.
-//
-
-#include "job.hpp"
--- AlgoBowl/AlgoBowl/workStation.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  workStation.cpp
-//  AlgoBowl
-//
-//  Created by Tobias Pepper on 9/24/18.
-//  Copyright © 2018 CSCI406. All rights reserved.
-//
-
-#include "workStation.hpp"
