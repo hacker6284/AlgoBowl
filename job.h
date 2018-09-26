b133a0df8381226bd658b66d0c63215ed55cca7a
ztmills <hacker.6284@gmail.com>
2018-09-26 11:23:39 -0600

    Added basic job class declaration in job.h

---
+++ job.h
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class job{
+private:
+  int availableTime;
+  int *stationTimes;
+  bool *completedStations;
+  int currentJob;
+
+public:
+  void setAvailableTime(int time);
+  void setStationTimes(int *times);
+
+  int getAvailableTime();
+  int *getStationTimes();
+  int getStationTime(int station);
+  bool *getCompletedStations();
+  bool getCompletedStation(int station);
+  int getCurrentJob();
+
+}
